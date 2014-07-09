3498065ecb6b467a51924e43309f22dea7607ac9
Kazuya MIURA <miup.0627@gmail.com>
2014-07-09 16:14:26 +0900

    きちんと成り立つ連立方程式になっていなかったバグを修正 平行移動、一致、係数０を排除

--- SimultanEquatSolution/TKBSEQuestion.m
+++ SimultanEquatSolution/TKBSEQuestion.m
@@ -29,8 +29,10 @@
 - (void)makeSEQuestionWithMaxCoefficient:(NSInteger)maxCoefficient allowFraction:(BOOL)allowFraction
 {
     // 解として-9~9までのランダムの数値を作成
-    _solutionX = arc4random() % 19 -9;
-    _solutionY = arc4random() % 19 -9;
+    int x = (arc4random() % 19) -9;
+    int y = (arc4random() % 19) -9;
+    _solutionX = x;
+    _solutionY = y;
     NSInteger SE1 = 0;
     NSInteger SE2 = 0;
     do {
@@ -39,13 +41,13 @@
         _x2Coefficient = arc4random() % (maxCoefficient * 2 + 1) -maxCoefficient;
         _y2Coefficient = arc4random() % (maxCoefficient * 2 + 1) -maxCoefficient;
         
-        _constant1 = arc4random() % (maxCoefficient * 2 + 1) -maxCoefficient;
-        _constant2 = arc4random() % (maxCoefficient * 2 + 1) -maxCoefficient;
+        _constant1 = _x1Coefficient * _solutionX + _y1Coefficient * _solutionY;
+        _constant2 = _x2Coefficient * _solutionX + _y2Coefficient * _solutionY;
         
         SE1 = _x1Coefficient * _solutionX + _y1Coefficient * _solutionY - _constant1;
         SE2 = _x2Coefficient * _solutionX + _y2Coefficient * _solutionY - _constant2;
         
-    } while (SE1 != SE2);
+    } while (SE1 != SE2 || _x1Coefficient == 0 || _y1Coefficient == 0 || _x2Coefficient == 0 || _y2Coefficient == 0 || [self isParallelWithMaxCoefficient:maxCoefficient]);
     
     
 }
@@ -55,7 +57,20 @@
     //連立方程式の表示
     NSLog(@"SE1:%ldx + %ldy = %ld", _x1Coefficient, _y1Coefficient, _constant1);
     NSLog(@"SE2;%ldx + %ldy = %ld", _x2Coefficient, _y2Coefficient, _constant2);
+    NSLog(@"x = %ld, y = %ld", _solutionX, _solutionY);
     
+}
+
+//一致、または平行移動になっているかどうか
+- (BOOL) isParallelWithMaxCoefficient:(NSInteger)maxCoefficient
+{
+    
+    for (int i = (int)-maxCoefficient; i < maxCoefficient; i++) {
+        for (int j = (int)-maxCoefficient; j < maxCoefficient; j++) {
+            if (i * _x1Coefficient == j * _x2Coefficient && i * _y1Coefficient == _y2Coefficient && i != 0 && j != 0) return TRUE;
+        }
+    }
+    return false;
 }
 
 @end
